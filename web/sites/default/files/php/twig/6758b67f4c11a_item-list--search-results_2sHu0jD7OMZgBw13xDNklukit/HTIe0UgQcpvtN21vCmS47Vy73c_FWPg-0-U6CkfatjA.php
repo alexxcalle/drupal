c959a73ce74a0f97a1b14199e58dd55d6b476a32
alexxcalle <axcalle@sudamericano.edu.ec>
2024-12-10 22:58:33 -0500

    implement body

---
+++ web/sites/default/files/php/twig/6758b67f4c11a_item-list--search-results_2sHu0jD7OMZgBw13xDNklukit/HTIe0UgQcpvtN21vCmS47Vy73c_FWPg-0-U6CkfatjA.php
@@ -0,0 +1,156 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* themes/custom/mi_tema/bootstrap/templates/dataset/item-list--search-results.html.twig */
+class __TwigTemplate_8a57ccaafe25024484b2317f5cef9257 extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 25
+        $context["classes"] = ["list-group", (CoreExtension::getAttribute($this->env, $this->source,         // line 27
+($context["context"] ?? null), "plugin", [], "any", false, false, true, 27) . "-results")];
+        // line 30
+        $context["attributes"] = CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 30);
+        // line 31
+        yield "
+";
+        // line 32
+        if (CoreExtension::getAttribute($this->env, $this->source, ($context["context"] ?? null), "list_style", [], "any", false, false, true, 32)) {
+            // line 33
+            $context["attributes"] = CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [("item-list__" . CoreExtension::getAttribute($this->env, $this->source, ($context["context"] ?? null), "list_style", [], "any", false, false, true, 33))], "method", false, false, true, 33);
+        }
+        // line 35
+        if ((($context["items"] ?? null) || ($context["empty"] ?? null))) {
+            // line 36
+            if ( !Twig\Extension\CoreExtension::testEmpty(($context["title"] ?? null))) {
+                // line 37
+                yield "<h3>";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true);
+                yield "</h3>";
+            }
+            // line 39
+            if (($context["items"] ?? null)) {
+                // line 40
+                yield "<";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["list_type"] ?? null), "html", null, true);
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["attributes"] ?? null), "html", null, true);
+                yield ">";
+                // line 41
+                $context['_parent'] = $context;
+                $context['_seq'] = CoreExtension::ensureTraversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 42
+                    yield "<li";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, $context["item"], "attributes", [], "any", false, false, true, 42), "addClass", ["list-group-item"], "method", false, false, true, 42), "html", null, true);
+                    yield ">";
+                    yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, $context["item"], "value", [], "any", false, false, true, 42), "html", null, true);
+                    yield "</li>";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_key'], $context['item'], $context['_parent']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 44
+                yield "</";
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["list_type"] ?? null), "html", null, true);
+                yield ">";
+            } else {
+                // line 46
+                yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ($context["empty"] ?? null), "html", null, true);
+            }
+        }
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["context", "items", "empty", "title", "list_type"]);        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "themes/custom/mi_tema/bootstrap/templates/dataset/item-list--search-results.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  92 => 46,  87 => 44,  77 => 42,  73 => 41,  68 => 40,  66 => 39,  61 => 37,  59 => 36,  57 => 35,  54 => 33,  52 => 32,  49 => 31,  47 => 30,  45 => 27,  44 => 25,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "themes/custom/mi_tema/bootstrap/templates/dataset/item-list--search-results.html.twig", "/app/web/themes/custom/mi_tema/bootstrap/templates/dataset/item-list--search-results.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 25, "if" => 32, "for" => 41);
+        static $filters = array("escape" => 37);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'for'],
+                ['escape'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
